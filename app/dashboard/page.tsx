9693fa1f6e9ba88aa8e23ea426a6c593d4714bc6
amrit <amrit882622gmail.com>
2026-09-24 17:10:45 +0530

    dashboard layout

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const DashboardPage = () => {
+  return (
+    <div>
+        {/* <UserButton/> */}
+        <h2>Mai main dash board hoon</h2>
+    </div>
+  )
+}
+
+export default DashboardPage
--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,12 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+const isProdectedRoute = createRouteMatcher([
+  "/dashboard/:path*",
+  "/dashboard"
+])
+export default clerkMiddleware(async(auth, req) => {
+  if(isProdectedRoute(req)) await auth.protect();
+});
 
 export const config = {
   matcher: [
